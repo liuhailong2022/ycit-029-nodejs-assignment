7c14cbb631638206acfed453fc0c6d2682ef708c
liuhailong2022 <liuhailong2022@gmail.com>
2022-12-15 00:22:35 -0500

    complete the assignment

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
-const express = require("express");
+const express = require('express')
 
-const app = express();
+const app = express()
 
 const data = [
-  {
-    id: 1,
-    name: "John Doe",
-    age: 32,
-  },
-  {
-    id: 2,
-    name: "Jane Doe",
-    age: 30,
-  },
-  {
-    id: 3,
-    name: "John Smith",
-    age: 25,
-  },
-];
+    {
+        id: 1,
+        name: 'John Doe',
+        age: 32,
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        age: 30,
+    },
+    {
+        id: 3,
+        name: 'John Smith',
+        age: 25,
+    },
+]
 
 // This route gets *ALL* the users
-app.get("/api/users", (req, res) => {
-  res.json(data);
-});
+
+app.get('/api/users', (req, res) => {
+    // get single user with query param
+    const user = data.filter((user) => user.id == req.query.id)
+    res.json(user)
+})
 
 // Add a new route to get a *SINGLE* user (you can use either path param or query param)
 // /api/users/1      <-- path param (req.params.id)
@@ -31,6 +34,43 @@ app.get("/api/users", (req, res) => {
 
 // BONUS QUESTION - Add routes to implement all the CRUD operations (POST, PUT, DELETE)
 
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/api/users', (req, res) => {
+    // add a new user
+    const user = {
+        id: data.length + 1,
+        name: req.body.name,
+        age: req.body.age,
+    }
+    data.push(user)
+    res.json(user)
+})
+
+app.put('/api/users/:id', (req, res) => {
+    // update a user
+    const user = data.find((user) => user.id === parseInt(req.params.id))
+    if (!user) {
+        res.status(404).send('The user with the given ID was not found.')
+        return
+    }
+    user.name = req.body.name
+    user.age = req.body.age
+    res.json(user)
+})
+
+app.delete('/api/users/:id', (req, res) => {
+    // delete a user
+    const user = data.find((user) => user.id === parseInt(req.params.id))
+    if (!user) {
+        res.status(404).send('The user with the given ID was not found.')
+        return
+    }
+    const index = data.indexOf(user)
+    data.splice(index, 1)
+    res.json(user)
+})
+
 app.listen(3000, () => {
-  console.log("Example app listening on port 3000!");
-});
+    console.log('Example app listening on port 3000!')
+})
